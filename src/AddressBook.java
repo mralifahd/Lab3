5c5d6a9eef95b30dff348b95c0ed9eabe2507b02
alifahd <alifahd@cmail.carleton.ca>
2020-09-25 00:40:01 -0400

    adding add and remove buddy methods

---
+++ src/AddressBook.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyInfoList;
+
+    public AddressBook() {
+        this.buddyInfoList = new ArrayList<BuddyInfo>();
+    }
+
+    /**
+     *
+     * Adds BuddyInfo object to the Address Book
+     *
+     * @param b
+     */
+    public void addBuddy(BuddyInfo b) {
+        buddyInfoList.add(b);
+    }
+
+    /**
+     *
+     * Removes BuddyInfo object from the Address Book
+     *
+     * @param b
+     */
+    public void removeBuddy(BuddyInfo b) {
+        for(BuddyInfo buddy : this.buddyInfoList) {
+            if(buddy.equals(b)) {
+                this.buddyInfoList.remove(buddy);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Address Book: ");
+    }
+}
